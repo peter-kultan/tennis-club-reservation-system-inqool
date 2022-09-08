1b77e9ed1f141018caf6603f8419253636476f90
Peter Kultán <xkultan@mail.muni.cz>
2022-09-08 09:45:30 +0200

    feat: add CourtService

---
+++ src/main/java/cz/services/CourtService.java
@@ -0,0 +1,32 @@
+package cz.services;
+
+import cz.models.Court;
+
+import java.util.Collection;
+
+public class CourtService implements Service<Court> {
+    @Override
+    public Collection<Court> getAllEntities() {
+        return null;
+    }
+
+    @Override
+    public Collection<Court> getEntity(int id) {
+        return null;
+    }
+
+    @Override
+    public void addEntity(Court court) {
+
+    }
+
+    @Override
+    public void updateEntity(Court court) {
+
+    }
+
+    @Override
+    public void deleteEntity(int id) {
+
+    }
+}
