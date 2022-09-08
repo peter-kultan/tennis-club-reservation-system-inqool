68c88495e6531307018708bbca824e625f9607d5
Peter Kultán <xkultan@mail.muni.cz>
2022-09-08 20:27:09 +0200

    feat: add surface controller

---
+++ src/main/java/cz/controllers/SurfaceController.java
@@ -0,0 +1,47 @@
+package cz.controllers;
+
+import cz.models.Surface;
+import cz.services.SurfaceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/surfaces")
+public class SurfaceController {
+
+    @Autowired
+    private SurfaceService surfaceService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Surface> getSurfaceById(@PathVariable(name = "id") Integer id) {
+        return new ResponseEntity<>(surfaceService.getSurfaceById(id), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Surface>> getAllSurfaces() {
+        return new ResponseEntity<>(surfaceService.getAllSurfaces(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Surface> addSurface(@RequestBody Surface newSurface) {
+        surfaceService.addSurface(newSurface);
+        return new ResponseEntity<>(newSurface, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Surface> updateSurface(@RequestBody Surface surface) {
+        surfaceService.updateSurface(surface);
+        return new ResponseEntity<>(surface, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteSurface(@PathVariable(name = "id") Integer id) {
+        surfaceService.deleteSurface(id);
+        // TODO: return
+        return null;
+    }
+}
