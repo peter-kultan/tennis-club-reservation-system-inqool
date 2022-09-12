50c7e7bf14149c59142558e33f43dc18a4ca7f20
Peter Kultán <xkultan@mail.muni.cz>
2022-09-12 21:52:31 +0200

    feat: add javaTimeModule to serialize and deserialize LocalDateTime

--- src/main/java/cz/Main.java
+++ src/main/java/cz/Main.java
@@ -1,7 +1,9 @@
 package cz;
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
@@ -9,5 +11,10 @@ public class Main {
     public static void main(String[] args)
     {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public JavaTimeModule javaTimeModule() {
+        return new JavaTimeModule();
     }
 }
